e94e74ae4cace5ff08f752bb02a34318ee67ae32
AbinayaArumugam <47531319+AbinayaArumugam@users.noreply.github.com>
2021-09-20 10:43:22 +0530

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -83,6 +83,51 @@ public class Runner {
                String str13=in.nextLine();
                System.out.println(LastWord.printLastWord(str13));
                break;
+               case 14:
+              System.out.println("Enter the String");
+              in.nextLine();
+              String str14=in.nextLine();
+              System.out.println(RemovePalindrome.removePalindrome(str14));
+              break;
+          case 15:
+              System.out.println("Enter the String");
+              String str15=in.next();
+              System.out.println(Encoding.decoding(str15));
+          case 16:
+              System.out.println("Enter the String");
+              String str16=in.nextLine();
+              System.out.println(Panagram.findPanagram(str16));
+              break;
+          case 17:
+              System.out.println("Enter the String");
+              String string1=in.next();
+              System.out.println("Enter the second String ");
+              String string2=in.next();
+              System.out.println(Isometric.areIsometric(string1,string2));
+              break;
+          case 18:
+              System.out.println("Enter the String");
+              String str18=in.next();
+              char[] expr=str18.toCharArray();
+              System.out.println(BalancedParenthesis.check(expr,expr.length));
+              break;
+          case 19:
+              System.out.println("Enter the String");
+              String str19=in.nextLine();
+              System.out.println(FirstLetter.findFirstLetter(str19));
+              break;
+          case 20:
+              System.out.println("Enter the String");
+              String stk1=in.next();
+              System.out.println("Enter the second String ");
+              String stk2=in.next();
+              System.out.println(RemoveWord.removeString(stk1,stk2));
+              break;
+          case 21:
+              System.out.println("Enter the String");
+              String str21=in.next();
+              System.out.println(ReverseVowel.reverseVowel(str21));
+              break;
           default :
               System.out.println("Invalid");
               break;
